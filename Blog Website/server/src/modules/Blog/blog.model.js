2bb733705f2dea5ed5dac80a07a8b637395d7b9f
Rohitbora07 <rohitbora098@gmail.com>
2026-06-20 18:55:28 +0530

    feat: blog model is completed

---
+++ Blog Website/server/src/modules/Blog/blog.model.js
@@ -0,0 +1,81 @@
+import mongoose from 'mongoose';
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    coverImage: {
+        url: {
+            type: String,
+            required: true
+        },
+        publicId: {
+            type: String,
+            required: true
+        }
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['draft', 'published'],
+        default: 'draft'
+    },
+    publishedAt: {
+        type: Date,
+        default: null
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    bookmarks: {
+        type: Number,
+        default: 0
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    commentsCount: {
+        type: Number,
+        default: 0
+    },
+    allowComments: {
+        type: Boolean,
+        default: true
+    },
+},{timestamps: true});
+
+blogSchema.index({author: 1});
+blogSchema.index({ category: 1 });
+blogSchema.index({ tags: 1 });
+blogSchema.index({ createdAt: -1 });
+
+const Blog = mongoose.models.Blog || mongoose.model('Blog', blogSchema);
+
+export default Blog;
